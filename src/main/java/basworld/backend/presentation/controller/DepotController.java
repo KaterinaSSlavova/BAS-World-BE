00d1b3075f990296f5c7737c0eecd03227a2583a
Amalia Gorgan <a.gorgan@student.fontys.nl>
2026-04-08 18:58:32 +0200

    changed controller to restcontroller in the depotcontroller

--- src/main/java/basworld/backend/presentation/controller/DepotController.java
+++ src/main/java/basworld/backend/presentation/controller/DepotController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/depots")
 @RequiredArgsConstructor
 public class DepotController {
