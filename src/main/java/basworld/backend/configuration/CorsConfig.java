5b5e48543fcd2b3833aa93ec9a4270a660d58598
KaterinaSSlavova <k.slavova@student.fontys.nl>
2026-04-13 12:13:21 +0200

    fixing cors configuration

--- src/main/java/basworld/backend/configuration/CorsConfig.java
+++ src/main/java/basworld/backend/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://145.220.72.89")
+                        .allowedOrigins("http://localhost:5173", "http://145.220.72.89", "http://145.220.72.89:3000")
                         .allowedMethods("*").allowedHeaders("*");
             }
         };
