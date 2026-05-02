0cb2a55f4a4235dc44cd79cbb27e7c3513bbf49d
KaterinaSSlavova <k.slavova@student.fontys.nl>
2026-05-02 21:12:03 +0200

    fix test profile for integration tests

--- src/main/java/basworld/backend/presentation/controller/BrandController.java
+++ src/main/java/basworld/backend/presentation/controller/BrandController.java
@@ -9,14 +9,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/brands")
-@ActiveProfiles("test")
 @RequiredArgsConstructor
 public class BrandController {
     private final CreateBrandUseCase createBrandUseCase;
