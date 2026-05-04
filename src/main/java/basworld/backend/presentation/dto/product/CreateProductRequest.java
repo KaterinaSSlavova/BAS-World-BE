37473dfcdbcf59950d85f926bce909341b6eb8b8
I545577 <I545577@fontysict.nl>
2026-05-04 17:21:04 +0200

    Added GlobalExceptionHandler and ApiError, did unit tests and controller tests regarding product

--- src/main/java/basworld/backend/presentation/dto/product/CreateProductRequest.java
+++ src/main/java/basworld/backend/presentation/dto/product/CreateProductRequest.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @AllArgsConstructor @Data
