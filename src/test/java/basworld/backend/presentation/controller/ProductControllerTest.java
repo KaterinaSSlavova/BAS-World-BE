fa74f77a635418754a73c9f7afc72c442e3a915d
I545577 <I545577@fontysict.nl>
2026-05-06 16:40:56 +0200

    Merge remote-tracking branch 'origin/main' into feature/product-depot-config

--- src/test/java/basworld/backend/presentation/controller/ProductControllerTest.java
+++ src/test/java/basworld/backend/presentation/controller/ProductControllerTest.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
