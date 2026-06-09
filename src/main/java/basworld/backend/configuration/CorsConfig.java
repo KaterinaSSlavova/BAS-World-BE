2509e19662ba64c557b62f178f07b913e9b78616
Slavova,Katerina K.S. <k.slavova@student.fontys.nl>
2026-06-09 15:01:13 +0000

    Edit CorsConfig.java

--- src/main/java/basworld/backend/configuration/CorsConfig.java
+++ src/main/java/basworld/backend/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://145.220.72.89", "http://145.220.72.89:3000")
+                        .allowedOrigins("http://localhost:5173", "http://145.220.72.89", "http://145.220.72.89:3000", "https://bas-world-fe-production.up.railway.app")
                         .allowedMethods("*").allowedHeaders("*");
             }
         };
